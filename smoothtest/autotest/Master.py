7bc37c4516469f0ce6c905a81865d4ad6fb0aab5
jduo <joaduo@gmail.com>
2014-12-20 05:02:35 -0300

    Fix style.

--- smoothtest/autotest/Master.py
+++ smoothtest/autotest/Master.py
@@ -76,10 +76,10 @@ class Master(ChildBase):
         master, watcher = multiprocessing.Pipe(duplex=False)
         self._m_w_conn = master
         self._w_m_conn = watcher
-        
+
         #poll inputs
         self._poll_sockets = []
-        self._poll_timeout = 0        
+        self._poll_timeout = 0
         #select inputs
         self._select_args = {}
         self._restart_lock = threading.Lock()
@@ -87,33 +87,33 @@ class Master(ChildBase):
         self._io_blacklist = set()
         if not self._parent_conn:
             self._io_blacklist.add(self.parent_conn)
-            
+
     def parent_conn(self):
         return self._parent_conn
-    
+
     def slave_conn(self):
         return self._slave.get_conn()
-    
+
     def m_w_conn(self):
         return self._m_w_conn
-        
+
     def set_select_args(self, **select_args):
         self._select_args = select_args
 
     def set_poll_args(self, sockets, timeout=0):
         self._poll_sockets = sockets
         self._timeout = timeout
-    
+
     def io_loop(self, test_config, poll=None, select=None, block=True):
         #manager of the subprocesses
         self._slave.start_subprocess()
 
         #create callback for re-testing on changes/msgs
         self.new_test(**test_config)
-        
+
         #build the block function listening to events
         get_event = self._build_get_event(poll, select)
-        
+
         self.wait_input = True
         #loop listening events
         while self.wait_input:
@@ -145,7 +145,7 @@ class Master(ChildBase):
                        (list(test_paths), path))
             #to force reloading all modules we directly kill and restart
             #the process
-            with self._restart_lock: #locking is just in case of being bombed
+            with self._restart_lock:  # locking is just in case of being bombed
                 self._watcher.unwatch_all()
                 self.restart_subprocess()
                 self._watcher.start_observer()
@@ -162,19 +162,19 @@ class Master(ChildBase):
             (instead of calling the tests callbacks within the thread)
             '''
             self._w_m_conn.send(self.cmd(self.partial_callback, path))
-        
+
         def full_msg(path):
             '''
             File _watcher thread -> master thread msg
             We use pipes to avoid race conditions on other IO mechanism
             (instead of calling the tests callbacks within the thread)
-            '''        
+            '''
             self._w_m_conn.send(self.cmd(self.full_callback, path))
 
         self._watcher.unwatch_all(clear=True)
         for ppath in partial_reloads:
             self._watcher.watch_file(ppath, partial_msg)
-            
+
         full_filter = self._build_path_filter(partial_reloads, full_filter)
         for fpath in full_reloads:
             self._watcher.watch_recursive(fpath, full_msg,
@@ -201,9 +201,10 @@ class Master(ChildBase):
             pfilter = path_filter
 
         partial_reloads = set(realPath(p) for p in partial_reloads)
+
         def _path_filter(path):
             return pfilter(path) and realPath(path) not in partial_reloads
-        
+
         return _path_filter
 
     def restart_subprocess(self):
@@ -221,7 +222,7 @@ class Master(ChildBase):
             rlist = set(self._io_list) - self._io_blacklist
             rlist = [conn().fileno() for conn in rlist]
             return rlist
-        
+
         #If not set, set the io wait method
         select = select_mod.select if not(poll or select) else select
 
@@ -230,22 +231,22 @@ class Master(ChildBase):
                 #in interactive mode we need to listen to stdin
                 sockets = lists_to_sockets(local_rlist(), [], [])
                 return sockets + self._poll_sockets
-            
+
             def get_event():
                 sockets = poll(build_sockets())
                 sockets, (rlist, _, _) = filter_sockets(sockets, local_rlist())
                 return bool(sockets), sockets, rlist
-            
+
         elif select:
             def build_rlist():
                 #in interactive mode we need to listen to stdin
-                return local_rlist() + list(self._select_args.get('rlist',[]))
-    
+                return local_rlist() + list(self._select_args.get('rlist', []))
+
             def get_event():
                 rlist = build_rlist()
                 lrlist = local_rlist()
-                wlist = self._select_args.get('wlist',[])
-                xlist = self._select_args.get('xlist',[])
+                wlist = self._select_args.get('wlist', [])
+                xlist = self._select_args.get('xlist', [])
                 timeout = self._select_args.get('timeout')
                 if timeout:
                     rlist, wlist, xlist = select(rlist, wlist, xlist, timeout)
@@ -262,12 +263,13 @@ class Master(ChildBase):
     def _dispatch(self, rlist):
         #Build a dispatch dictionary
         #{conn_func:dispatch_lambda} dict
-        fdict = {self.slave_conn: (lambda:self._recv_slave()),
-             self.m_w_conn:(lambda:self._dispatch_cmds(self._m_w_conn, False)),
-             self.parent_conn:(lambda:self._dispatch_cmds(self._parent_conn)),
+        fdict = {self.slave_conn: (lambda: self._recv_slave()),
+             self.m_w_conn: (lambda: self._dispatch_cmds(self._m_w_conn, False)),
+             self.parent_conn: (lambda: self._dispatch_cmds(self._parent_conn)),
              }
         #Convert to {fileno:(dispatch_lambda, conn_func)...} dict
-        fnodict = dict( (f().fileno(), (lb,f) ) for f,lb in fdict.items() if f())
+        fnodict = dict((f().fileno(), (lb, f)) for f, lb in fdict.items()
+                        if f())
         #dispatch events
         for f in rlist:
             try:
@@ -292,14 +294,14 @@ class Master(ChildBase):
         first = self._slave._first_test
         answer = self._slave.recv_answer()
         if answer.error and not first:
-            self.full_callback('Error on non-first run')        
+            self.full_callback('Error on non-first run')
 
 
 def smoke_test_module():
     test_paths = ['smoothtest.tests.example.test_Example.Example.test_example']
     partial_reloads = ['MasterAutoTest.py']
     mat = Master()
-    poll = mat.io_loop(dict(test_paths=test_paths, 
+    poll = mat.io_loop(dict(test_paths=test_paths,
                             partial_reloads=partial_reloads, smoke=True),
                        block=False)
     for _ in poll:
