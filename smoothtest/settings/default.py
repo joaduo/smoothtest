91efffaac2165abcf804467ace2e6e6ce748bca7
jduo <jduo@juju.com>
2017-01-21 03:09:33 -0300

    Share config with xpathwebdriver

--- smoothtest/settings/default.py
+++ smoothtest/settings/default.py
@@ -6,17 +6,10 @@ Copyright (c) 2014 Juju. Inc
 Code Licensed under MIT License. See LICENSE file.
 '''
 import logging
+from xpathwebdriver.levels import INMORTAL_LIFE, PROCESS_LIFE,\
+    TEST_RUNNER_LIFE, TEST_ROUND_LIFE, SINGLE_TEST_LIFE
 
-# Webdriver life level
-#GOD_LIFE = 7 # Browser survives across universes (WIP)
-#AI_LIFE = 6 # Browser survives across reboots and becomes agent smith (WIP)
-INMORTAL_LIFE = 5 # Browser survives across processes
-PROCESS_LIFE = 4 # Browser is alive as long as the smoothtest process is running
-TEST_RUNNER_LIFE = 3 # Browser is alive as long as the TestRunner process is running (new test runner process means new browser) 
-TEST_ROUND_LIFE = 2 # Browser is alive for a single test round
-SINGLE_TEST_LIFE = 1 # Start 1 new browser per test ran
-
-class DefaultSettings(object):
+class Settings(object):
     # Signal that we are working on production. To disable write tests
     production = False
 
@@ -61,7 +54,7 @@ class DefaultSettings(object):
 
 
 def smoke_test_module():
-    DefaultSettings()
+    Settings()
 
 if __name__ == "__main__":
     smoke_test_module()
